4122233a300a92f3a6723ae8236df03f9b60b8e8
ezgi.colak <ezgi.colak@YAZLIMPC-03>
2022-09-20 11:13:37 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+ 
+    public class Context:DbContext
+    {
+        public Context()
+        {
+            Database.Connection.ConnectionString = "server=devsql2019;database=Ezgi_TestDb;user=posuser;password=posuser";
+        }
+        
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
