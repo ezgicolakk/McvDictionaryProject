0db717606632b9c0fe732a23c41f9317f28de4a7
Ezgi ÇOLAK <ezgi.colak@konfides.com>
2022-09-22 18:02:07 +0300

    ICategoryService added. CategoryManager Updated. EntityFramework folder added to DataAccessLayer. EfAboutDal, EfCategoryDal, EfContactDal, EfContentDal, EfHeadingDal, EfWriterDal added. CategoryController updated.

--- McvProject/Controllers/CategoryController.cs
+++ McvProject/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -11,14 +12,14 @@ namespace McvProject.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
-        CategoryManager cm = new CategoryManager();
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult GetCategoryList()
         {
-            var categoryvalues = cm.GetAllBL();
+            var categoryvalues = cm.GetList();
             return View(categoryvalues);
         }
         [HttpGet]
@@ -29,7 +30,7 @@ namespace McvProject.Controllers
         [HttpPost]
         public ActionResult AddCategory(Category p)
         {
-            cm.CategoryAddBL(p);
+            //cm.CategoryAddBL(p);
             return RedirectToAction("GetCategoryList");
         }
     }
